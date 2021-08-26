4f8f73c0dc3c1ed72291c635fc986c6f19f67829
Abhishek Bhatkar <54368008+abhishek-bhatkar@users.noreply.github.com>
2021-08-26 07:35:48 +0000

    Add logic for question and answer to process

--- index.js
+++ index.js
@@ -29,9 +29,46 @@ var questions = [
 // welcome function
 function welcome() {
 userName = readLineSync.question("Hi, What's your name? ");
-
+console.log("---#---#---#---");
 console.log("Welcome "+ userName + " to Iron man quizz!");
+console.log("---#---#---#---");
+}
+// play function
+function play(question, answer) {
+  var userAnswer = readLineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) { // branching
+    console.log("right!");
+    score = score + 1;
+  } else if(answer.toUpperCase().includes(userAnswer.toUpperCase())){ 
+    // check for word
+    console.log("right!");
+    score = score + 1;
+  } else {
+    console.log("Wrong answer!");
+   
+  }
+
+  console.log("current score: ", score);
+  console.log("---#---#---#---");
+}
+
+// game function
+function game() {
+  for (var i=0; i<questions.length; i++) {
+    var currentQuestion = questions[i];
+    play(currentQuestion.question, currentQuestion.answer)
+  }
+}
+
+// score function
+function showScores() {
+  console.log("You SCORED: ", score);
+
 }
 
 // calling functions
+
 welcome();
+game();
+showScores();
